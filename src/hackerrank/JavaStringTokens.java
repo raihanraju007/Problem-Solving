1f0ac0e7842ee438bc48a663ef2d9c7fd35ed3a5
raihanraju007 <raihanraju007@gmail.com>
2023-03-10 09:14:57 +0600

    Hacker Rank Java String Token

---
+++ src/hackerrank/JavaStringTokens.java
@@ -0,0 +1,23 @@
+package hackerrank;
+
+import java.util.Scanner;
+
+public class JavaStringTokens {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String s = scan.nextLine();
+        scan.close();
+
+        s = s.trim();
+        if (s.length()==0){
+            System.out.println(0);
+            return;
+        }
+
+        String [] words = s.split("[^a-zA-Z]+");
+        System.out.println(words.length);
+
+        for (String word : words)
+            System.out.println(word);
+    }
+}
